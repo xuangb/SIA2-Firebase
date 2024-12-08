741695484e48d9903f9cae0af67ee774161ca362
xuangb <xuangb@users.noreply.github.com>
2024-12-08 09:38:26 +0000

    deploy: f51e8859b328c397bd307a40cd6aaccaabd479f7

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,28 +1,48 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-// import { getAuth, GoogleAuthProvider} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-// import { getDatabase } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js"; // Import Firebase Realtime Database (if needed)
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDTFxn7j6qomVA59Mu-pK3I--gcr7gvRy0",
-  authDomain: "sia2-firebase.firebaseapp.com",
-  projectId: "sia2-firebase",
-  storageBucket: "sia2-firebase.firebasestorage.app",
-  messagingSenderId: "711179837759",
-  appId: "1:711179837759:web:bfde1b1b8b44df663ff38a",
-  measurementId: "G-1SCQNEMYYY"
-};
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+ import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+ import {getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
 
-firebase.initializeApp(firebaseConfig);
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyDjh1vEFk7FqW-wLaUE4pXowHf266_3W88",
+   authDomain: "sia-2develo4.firebaseapp.com",
+   projectId: "sia-2develo4",
+   storageBucket: "sia-2develo4.firebasestorage.app",
+   messagingSenderId: "853876043675",
+   appId: "1:853876043675:web:58e9a688aeef18690f3b3e"
+ };
 
-const auth = firebase.auth();
-const database = firebase.database();
+ // Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-function register() {
-  fullname = document.getElementById("fullname").value;
-  email = document.getElementById("email").value;
-  phoneNumber = document.getElementById("phoneNumber").value; // Corrected this line
-  password = document.getElementById("password").value;
-  confirmPassword = document.getElementById("confirm-password").value;
+
+function showMessage(message, elementId) {
+  var message = document.getElementById(elementId);
+  message.style.display='block';
+  message.innerHTML = message;
+  message.style.opacity = 1;
+  setTimeout(function(){
+    message.style.opacity = 0;
+  },5000);
+  
+}
+
+
+const signUp = document.getElementById("register");
+signUp.addEventListener('click',(event)=>{
+  event.preventDefault();
+  const fullname = document.getElementById("fullname").value;
+  const email = document.getElementById("email").value;
+  const phoneNumber = document.getElementById("phoneNumber").value; // Corrected this line
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirm-password").value;
+
+  const auth = getAuth();
+  const db = getFirestore();
 
   // Validate inputs
   if (!validate_email(email)) {
@@ -46,27 +66,43 @@ function register() {
     return;
   }
 
-  // Register user
-  auth
-    .createUserWithEmailAndPassword(email, password)
-    .then(function () {
-      var user = auth.currentUser;
-      var database_ref = database.ref();
 
-      var user_data = {
-        email: email,
-        fullname: fullname,
-        phoneNumber: phoneNumber,
-        last_login: Date.now(),
-      };
-      database_ref.child("users/" + user.uid).set(user_data);
 
-      alert("User created successfully.");
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredentials)=>{
+    const user = userCredentials.user;
+    const userData = {
+      email: email,
+      fullname: fullname,
+      phoneNumber: phoneNumber,
+      last_login: Date.now(),
+      role: "user",
+    };
+    showMessage("Account Created Successfully", "signUpMessage");
+
+    const docRef = doc(db,"users", user.uid);
+    setDoc(docRef, userData)
+    .then(()=>{
+      window.location.href = "/userpage.html";
     })
-    .catch(function (error) {
-      console.error("Error during registration:", error.message);
+    .catch((error)=>{
+      console.log("Error writing document");
     });
-}
+
+
+  })
+  .catch((error)=>{
+    const errorCode = error.code;
+    if(errorCode == "auth/email-already-in-use"){
+      showMessage("Email Address Already Exists", 'signUpMessage');
+    }else{
+      showMessage("Unable to create user", "signUpMessage");
+    }
+  })
+  
+
+})
+
 
 function validate_email(email) {
   const expression = /^[^@]+@\w+(\.\w+)+\w$/;
@@ -103,126 +139,3 @@ function validate_phoneNumber(phoneNumber) {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-// // Initialize Firebase
-
-// const auth = getAuth(app); 
-// auth.languageCode = 'en'; 
-// const provider = new GoogleAuthProvider();
-
-// // Initialize Realtime Database (if needed)
-// const database = getDatabase(app); // Use only if you need Firebase Database
-
-// // Google Sign-In Button Logic
-// const googleLogin = document.getElementById("google-login-btn");
-// googleLogin.addEventListener("click", function () {
-//   signInWithPopup(auth, provider)
-//     .then((result) => {
-//       // User signed in successfully
-//       const credential = GoogleAuthProvider.credentialFromResult(result);
-//       const user = result.user;
-//       console.log("User signed in:", user);
-      
-//       // Redirect user to another page
-//       window.location.href = "/userpage.html";
-
-//     })
-//     .catch((error) => {
-//       // Handle Errors
-//       console.error("Error during sign-in:", error.message);
-//       const errorCode = error.code;
-//       const errorMessage = error.message;
-//       const email = error.customData.email;
-//       const credential = GoogleAuthProvider.credentialFromError(error);
-//     });
-// });
-
-
-
-
-
-  // function register(){
-  //   fullname = document.getElementById("fullname").value;
-  //   email = document.getElementById("email").value;
-  //   password = document.getElementById("password").value;
-  //   confirmPassword = document.getElementById("confirm-password").value;
-
-  //   if(validate_email(email)==false || validate_password(password, confirmPassword) ==false){
-  //     alert("Either Email or Password is incorrect");
-  //     return
-  //   }
-
-  //   if(validate_field(fullname)==false){
-  //     alert("Name is empty");
-  //     return
-  //   }
-
-  //   auth.createUserWithEmailAndPassword(email,password){
-  //     .then(function(){
-  //       var user = auth.currentUser;
-  //       var database_ref = database.ref();
-
-  //       var user_data = {
-  //         email: email,
-  //         fullname: fullname,
-  //         last_login: Date.now(),
-  //       }
-
-  //       database_ref.child('users/'+user.uid).set(user_data);
-
-  //       alert("User created successfully");
-
-  //     })
-  // }
-
-  // function validate_email(email){
-  //   expression = /^[^@]+@\w+(\.\w+)+\w$/
-  //   if(expression.test(email)==true){
-  //     return true
-  //   }else{
-  //     return false
-  //   }
-  // }
-
-  // function validate_password(password, confirmPassword) {
-  //   const minLength = 8; // Minimum length of the password
-  //   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
-  
-  //   // Check if password and confirm password match
-  //   if (password !== confirmPassword) {
-  //     return "Passwords do not match.";
-  //   }
-  
-  //   // Check if password meets the minimum length requirement
-  //   if (password.length < minLength) {
-  //     return `Password must be at least ${minLength} characters long.`;
-  //   }
-  
-  //   // Check if password meets complexity requirements
-  //   if (!passwordRegex.test(password)) {
-  //     return "Password must include at least one uppercase letter, one lowercase letter, one number, and one special character.";
-  //   }
-  
-  //   return "Password is valid.";
-  // }
-
-  // function validate_field(field){
-  //   if(field == null){
-  //     return false;
-  //   }
-  //   if(field.length <= 0){
-  //     return false;
-  //   }else{
-  //     return true;
-  //   }
-  // }
